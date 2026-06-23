0d07095dbdccf26bf447464e3e8e8a9b41940337
Mukunda Madhava reddy k <mukundamadhavareddy540@gmail.com>
2026-06-23 17:54:21 +0530

    fix: switch SMTP from port 587 STARTTLS to port 465 SSL for Render compatibility

--- src/main/java/com/aurawear/util/EmailUtil.java
+++ src/main/java/com/aurawear/util/EmailUtil.java
@@ -41,9 +41,10 @@ public class EmailUtil {
 
         Properties props = new Properties();
         props.put("mail.smtp.host",            "smtp.gmail.com");
-        props.put("mail.smtp.port",            "587");
+        props.put("mail.smtp.port",            "465");
         props.put("mail.smtp.auth",            "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable",      "true");
+        props.put("mail.smtp.ssl.trust",       "smtp.gmail.com");
         props.put("mail.smtp.connectiontimeout", "10000");
         props.put("mail.smtp.timeout",           "10000");
 
@@ -112,9 +113,10 @@ public class EmailUtil {
                                              String shippingCity, String shippingState, String shippingPincode) throws MessagingException, java.io.UnsupportedEncodingException {
         Properties props = new Properties();
         props.put("mail.smtp.host",            "smtp.gmail.com");
-        props.put("mail.smtp.port",            "587");
+        props.put("mail.smtp.port",            "465");
         props.put("mail.smtp.auth",            "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable",      "true");
+        props.put("mail.smtp.ssl.trust",       "smtp.gmail.com");
         props.put("mail.smtp.connectiontimeout", "10000");
         props.put("mail.smtp.timeout",           "10000");
 
